b97bef4dba013c8e355747ba98357f867392827b
Guillermo <guillermo_848@hotmail.com>
2019-05-11 19:25:28 +0200

    premake update

--- premake5.lua
+++ premake5.lua
@@ -1,7 +1,10 @@
 project "ImGui"
     kind "StaticLib"
     language "C++"
-    
+    cppdialect "C++17"
+    staticruntime "on"
+
+
     targetdir ("bin/" .. outputdir .. "/%{prj.name}")
     objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
 
@@ -18,11 +21,14 @@ project "ImGui"
         "imstb_truetype.h",
         "imgui_demo.cpp"
     }
-    
+
     filter "system:windows"
         systemversion "latest"
-        cppdialect "C++17"
-        staticruntime "On"
-        
-    filter { "system:windows", "configurations:Release" }
-        buildoptions "/MT"
+
+	filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "on"
